057c254ea9403d7130c48c54b3bbe9dc47ae51d0
peter <phantrung696@gmail.com>
2022-10-22 16:16:30 +0700

    update: left menu selected + type collapse

---
+++ src/@App/Admin/components/Menu/LeftMenuItem.jsx
@@ -0,0 +1,46 @@
+/*
+ * Created Date: 22-10-2022, 3:15:13 pm
+ * Author: Peter
+ * Email: phantrung696@gmail.com
+ * -----
+ * Last Modified:
+ * Modified By:
+ * -----
+ * Copyright (c) 2022 PROS+ Group , Inc
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	----------------------------------------------------------
+ */
+
+import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import React from 'react'
+import { useMatch, useNavigate } from 'react-router-dom'
+// import PropTypes from 'prop-types'
+
+const LeftMenuItem = props => {
+	const { item, sx } = props
+	const navigate = useNavigate()
+	const match = useMatch(item?.url ?? '/')
+	// console.log('============= match', match)
+
+	const handleClickMenu = url => {
+		if (url) navigate(url)
+	}
+
+	return (
+		<ListItem disablePadding onClick={() => handleClickMenu(item?.url)} sx={sx} selected={Boolean(match)}>
+			<ListItemButton>
+				{item?.icon && <ListItemIcon>{item?.icon}</ListItemIcon>}
+
+				<ListItemText primary={item.title} />
+			</ListItemButton>
+		</ListItem>
+	)
+}
+
+//LeftMenuItem.defaultProps = {}
+
+//LeftMenuItem.propTypes = {}
+
+export default React.memo(LeftMenuItem)
