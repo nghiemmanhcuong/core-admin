acefc2c797c4820d9a41f48aa802de762c481b6a
Hai Tran <tranhoanglonghai2k@gmail.com>
2022-11-09 23:04:26 +0700

    fix bug redmine 9/11/2022

--- src/@App/Admin/pages/Event/Components/Tab3.jsx
+++ src/@App/Admin/pages/Event/Components/Tab3.jsx
@@ -1,24 +1,28 @@
 import React, { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { TRANSLATE_ADMIN } from '@App/Admin/configs/constants'
-import { Button, Paper, Typography, TextField, Box, Card } from '@mui/material'
+import {
+	Button,
+	Paper,
+	Typography,
+	TextField,
+	Box,
+	Card,
+	FormControlLabel,
+	TableFooter,
+	Pagination
+} from '@mui/material'
 import CoreInput from '@Core/components/Input/CoreInput'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import Yup from '@Core/helper/Yup'
-import CardActions from '@mui/material/CardActions'
-import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
-import { CoreActionDelete, CoreActionEdit, CoreActionView } from '@Core/components/Table/components/CoreTableAction'
-import FormAutocomplete from '@App/Admin/components/Form/FormAutocomplete'
-import CoreCheckbox from '@Core/components/Input/CoreCheckbox'
-import CoreRadioGroup from '@Core/components/Input/CoreRadioGroup'
+import { CoreActionDelete } from '@Core/components/Table/components/CoreTableAction'
 import Grid from '@mui/material/Grid'
 
 const FontTitle = ({ variant = 'h1', title = '' }) => {
@@ -37,67 +41,116 @@ const rows = [
 	createData(2, 'Ice cream sandwich', 237, 9.0, 7, 3, 1),
 	createData(3, 'Eclair', 262, 16.0, 2, 6, 1),
 	createData(1, 'Cupcake', 305, 3.7, 6, 4, 1),
-	createData(5, 'Gingerbread', 356, 16.0, 49, 9, 1)
+	createData(5, 'Gingerbread', 356, 16.0, 49, 9, 1),
+	createData(6, 'Cupcake', 305, 3.7, 67, 4.3),
+	createData(7, 'Donut', 452, 25.0, 51, 4.9),
+	createData(8, 'Eclair', 262, 16.0, 24, 6.0),
+	createData(9, 'Frozen yoghurt', 159, 6.0, 24, 4.0),
+	createData(10, 'Gingerbread', 356, 16.0, 49, 3.9),
+	createData(11, 'Honeycomb', 408, 3.2, 87, 6.5),
+	createData(12, 'Ice cream sandwich', 237, 9.0, 37, 4.3),
+	createData(13, 'Jelly Bean', 375, 0.0, 94, 0.0),
+	createData(14, 'KitKat', 518, 26.0, 65, 7.0),
+	createData(15, 'Lollipop', 392, 0.2, 98, 0.0),
+	createData(16, 'Marshmallow', 318, 0, 81, 2.0),
+	createData(17, 'Nougat', 360, 19.0, 9, 37.0),
+	createData(18, 'Oreo', 437, 18.0, 63, 4.0)
 ]
+
 const tableForm = () => {
+	const [page, setPage] = React.useState(0)
+	const [rowsPerPage, setRowsPerPage] = React.useState(10)
+
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage)
+	}
+
+	const handleChangeRowsPerPage = event => {
+		setRowsPerPage(+event.target.value)
+		setPage(0)
+	}
 	return (
-		<TableContainer component={Paper}>
-			<Table sx={{ minWidth: 650 }} aria-label="simple table">
-				<TableHead>
-					<TableRow>
-						<TableCell style={{ width: '12%' }}>
-							<Box sx={{ fontWeight: 'bold' }}>エントリーID</Box>
-						</TableCell>
-						<TableCell style={{ width: '20%' }}>
-							<Box sx={{ fontWeight: 'bold' }}>ユーザーID</Box>
-						</TableCell>
-						<TableCell style={{ width: '12%' }}>
-							<Box sx={{ fontWeight: 'bold' }}>ユーザー名</Box>
-						</TableCell>
-						<TableCell style={{ width: '14%' }}>
-							<Box sx={{ fontWeight: 'bold' }}>エントリー日時</Box>
-						</TableCell>
-						<TableCell style={{ width: '16%' }}>
-							<Box sx={{ fontWeight: 'bold' }}>エントリーカテゴリー</Box>
-						</TableCell>
-						<TableCell style={{ width: '16%' }}>
-							<Box sx={{ fontWeight: 'bold' }}>エントリー料</Box>
-						</TableCell>
-						<TableCell>
-							<Box sx={{ fontWeight: 'bold' }}>決済方法</Box>
-						</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{rows.map(row => (
-						<TableRow key={row.no}>
-							<TableCell>
-								<TextField type="number" id="outlined-search" size="small" defaultValue={row.no} />
+		<>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell style={{ width: '12%' }}>
+								<Box sx={{ fontWeight: 'bold' }}>エントリーID</Box>
 							</TableCell>
-							<TableCell>{row.name}</TableCell>
-							<TableCell>{row.fat}</TableCell>
-							<TableCell>{row.carbs}</TableCell>
-							<TableCell>
-								<TextField type="number" id="outlined-search" size="small" defaultValue={row.carbs1} />
+							<TableCell style={{ width: '20%' }}>
+								<Box sx={{ fontWeight: 'bold' }}>ユーザID</Box>
+							</TableCell>
+							<TableCell style={{ width: '12%' }}>
+								<Box sx={{ fontWeight: 'bold' }}>ユーザ名</Box>
+							</TableCell>
+							<TableCell style={{ width: '14%' }}>
+								<Box sx={{ fontWeight: 'bold' }}>エントリー日時</Box>
+							</TableCell>
+							<TableCell style={{ width: '16%' }}>
+								<Box sx={{ fontWeight: 'bold' }}>エントリーカテゴリ</Box>
+							</TableCell>
+							<TableCell style={{ width: '16%' }}>
+								<Box sx={{ fontWeight: 'bold' }}>エントリー料</Box>
 							</TableCell>
 							<TableCell>
-								<TextField
-									type="number"
-									id="outlined-search"
-									size="small"
-									defaultValue={row.protein1}
-								/>
-							</TableCell>
-							<TableCell>
-								<Box className="flex">
-									<CoreActionDelete onClick={() => console.log('============= data', data)} />
-								</Box>
+								<Box sx={{ fontWeight: 'bold' }}>決済方法</Box>
 							</TableCell>
 						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+					</TableHead>
+					<TableBody>
+						{rows.map(row => (
+							<TableRow key={row.no}>
+								<TableCell>
+									<TextField type="number" id="outlined-search" size="small" defaultValue={row.no} />
+								</TableCell>
+								<TableCell>{row.name}</TableCell>
+								<TableCell>{row.fat}</TableCell>
+								<TableCell>{row.carbs}</TableCell>
+								<TableCell>
+									<TextField
+										type="number"
+										id="outlined-search"
+										size="small"
+										defaultValue={row.carbs1}
+									/>
+								</TableCell>
+								<TableCell>
+									<TextField
+										type="number"
+										id="outlined-search"
+										size="small"
+										defaultValue={row.protein1}
+									/>
+								</TableCell>
+								<TableCell>
+									<Box className="flex">
+										<CoreActionDelete onClick={() => console.log('============= data', data)} />
+									</Box>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			<Box className="my-12 flex flex-row-reverse">
+				<Pagination
+					count={Math.ceil(rows?.length / 10) ?? 1}
+					variant="outlined"
+					shape="rounded"
+					onChange={handleChangeRowsPerPage}
+				/>
+			</Box>
+			{/* <TablePagination
+				rowsPerPageOptions={[10, 25, 100]}
+				component="div"
+				count={rows.length}
+				rowsPerPage={rowsPerPage}
+				page={page}
+				onPageChange={handleChangePage}
+				onRowsPerPageChange={handleChangeRowsPerPage}
+			/> */}
+		</>
 	)
 }
 const Tab3 = props => {
