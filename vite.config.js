3fef20eddc4c377d07712a9644895a83c537683d
elnavut <tulv@yopaz.vn>
2022-11-30 09:59:31 +0700

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import jsconfigPaths from 'vite-jsconfig-paths'
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		react(),
-		legacy({
-			targets: ['defaults', 'not IE 11']
-		}),
-		jsconfigPaths()
-	],
-	server: {
-		cors: true,
-		proxy: {
-			'/api': {
-				target: 'https://drake.aspr.work',
-				changeOrigin: true,
-				// rewrite: path => path.replace(/^\/api/, ''),
-				configure: (proxy, options) => {
-					// proxy will be an instance of 'http-proxy'
-				}
-			}
-		}
-	}
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [
+            react(),
+            legacy({
+                targets: ['defaults', 'not IE 11']
+            }),
+            jsconfigPaths()
+        ],
+        server: {
+            cors: true,
+            proxy: {
+                '/api': {
+                    target: env.BASE_URL,
+                    changeOrigin: true,
+                    // rewrite: path => path.replace(/^\/api/, ''),
+                    configure: (proxy, options) => {
+                        // proxy will be an instance of 'http-proxy'
+                    }
+                }
+            }
+        }
+    }
 })
