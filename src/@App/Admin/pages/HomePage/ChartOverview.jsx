19a016f6cd367cf88d45e8104012eb10ed0aad96
elnavut <tulv@yopaz.vn>
2022-12-27 14:58:08 +0700

    add chart in dashboard

--- src/@App/Admin/pages/HomePage/ChartOverview.jsx
+++ src/@App/Admin/pages/HomePage/ChartOverview.jsx
@@ -1,5 +1,5 @@
 /*
- * Created Date: 23-10-2022, 11:42:04 am
+ * Created Date: 23-10-2022, 11:40:14 am
  * Author: TheAnh58
  * Email: you@you.you
  * -----
@@ -13,39 +13,93 @@
  * ----------	---	----------------------------------------------------------
  */
 
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend } from 'chart.js'
 import { Chart } from 'react-chartjs-2'
 // import PropTypes from 'prop-types'
 import 'chart.js/auto'
-
+import { useRequest } from 'ahooks'
+import { errorMsg, successMsg } from '@Core/helper/Message'
+import { maintainceService } from '@App/Admin/services/maintainceService'
+import moment from 'moment/moment'
+import { CircularProgress } from '@mui/material'
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend)
 
+const labels = ['1', '2', '3', '4', '5', '6', '7']
+const colors = ['#ff5436', '#f77900', '#f9fa93', '#00ea00', '#199111', '#00b9c1', '#0000f9', '#910887']
+const colors1 = colors[Math.round(Math.random() * colors.length)]
+const colors2 = colors.filter(item => item !== colors1)[
+	Math.round(Math.random() * colors.filter(item => item !== colors1).length)
+]
 const ChartOverview = props => {
 	const chartRef = useRef(null)
-	const labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6']
-	const data = {
-		labels: labels,
-		datasets: [
-			{
-				label: 'Shose',
-				data: [30, 30, 60, 120, 70, 70],
-				borderColor: 'rgb(254,185,105)'
-			},
-			{
-				label: 'Suits',
-				data: [90, 180, 50, 70, 70, 30],
-				borderColor: 'rgb(251,99,64)'
-			},
-			{
-				label: 'Jeans',
-				data: [60, 190, 80, 170, 70, 80],
-				borderColor: 'rgb(119,100,228)'
+	const [data, setData] = useState({ labels, datasets: [] })
+
+	const {
+		data: dataSale,
+		run: getDataSale,
+		loading: loadingDataSale
+	} = useRequest(maintainceService.handleDownload, {
+		manual: true,
+		onSuccess: (result, params) => {
+			handleMapDataToChart(result)
+		},
+		onError: (res, params) => {
+			if (params) {
+				mutate({
+					data: []
+				})
+			} else {
+				errorMsg(res?.response?.data?.error_message)
 			}
-		]
+		}
+	})
+
+	const handeleGetDataSale = async () => {
+		const params = {
+			export_format: 'JSON',
+			collect_duration: 'all',
+			// collect_duration_from: '2022-08',
+			report_display: 'monthly',
+			export_app: ['wau']
+		}
+
+		await getDataSale(params)
 	}
 
-	return <Chart ref={chartRef} type="line" data={data} />
+	const handleMapDataToChart = async dataReponse => {
+		const dataWau = dataReponse?.export_app?.wau
+		if (!dataWau) {
+			setData({
+				labels: [],
+				datasets: []
+			})
+			return
+		}
+		const labelChart = []
+		const dataChartWau = []
+		dataWau.forEach(item => {
+			labelChart.push(`${moment(item.date_from).format('DD')}-${moment(item.date_until).format('DD/MM/YYYY')}`)
+			dataChartWau.push(item?.number)
+		})
+
+		setData({
+			labels: labelChart,
+			datasets: [
+				{
+					label: 'WAU',
+					data: dataChartWau,
+					borderColor: '#00ea00'
+				}
+			]
+		})
+	}
+
+	useEffect(() => {
+		handeleGetDataSale()
+	}, [])
+
+	return loadingDataSale ? <CircularProgress /> : <Chart ref={chartRef} type="line" data={data} />
 }
 
 // ChartOverview.defaultProps = {}
